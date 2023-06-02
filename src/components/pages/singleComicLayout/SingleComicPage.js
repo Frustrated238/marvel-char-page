a0aea1059bb2059b59c289b8a0674420cd5573c8
Mykola Olinyk <mikola.oliinik@gmail.com>
2023-06-03 01:52:34 +0300

    Adaptatation for tablets

--- src/components/pages/singleComicLayout/SingleComicPage.js
+++ src/components/pages/singleComicLayout/SingleComicPage.js
@@ -20,7 +20,7 @@ const SingleComicLayout = ({data}) => {
             <div className="single-comic__info">
                 <h2 className="single-comic__name">{title}</h2>
                 <p className="single-comic__descr">{description}</p>
-                <p className="single-comic__descr">{pageCount}</p>
+                <p className="single-comic__descr">Pages: {pageCount}</p>
                 <p className="single-comic__descr">Language: {language}</p>
                 <div className="single-comic__price">{price}</div>
             </div>
